074260e5db84c4becd79450d5c96500f7341dd86
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-10 07:33:17 -0500

    Cleaned up program class

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -10,19 +10,8 @@ namespace CustomList
     {
         static void Main(string[] args)
         {
-            //ListMethod<T> listMethods = new ListMethod<T>();
-            ListMethod<int> myList = new ListMethod<int>();
-            myList.Add(1);
-            myList.Add(2);
-            myList.Add(3);
-            myList.Add(4);
-            myList.Add(5);
-            myList.Add(6);
-            foreach (int item in myList)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
+            ////Syntax as follows for custom list!
+            //ListMethod<int> myList = new ListMethod<int>();
         }
     }
 }
